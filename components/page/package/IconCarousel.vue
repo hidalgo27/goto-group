25faf1c6e91dfcb9d83065229f0d79598cf6b1ca
Julio <julio@goto.group>
2025-01-21 14:47:40 -0500

    fix: Change the img to svg and hide the arrows when the device has less than 768px in IconCarousel

--- components/page/package/IconCarousel.vue
+++ components/page/package/IconCarousel.vue
@@ -19,36 +19,70 @@ const breakpoints = {
     snapAlign: 'start',
   },
 }
+
 </script>
 
 <template>
   <section class="bg-secondary  bg-opacity-10 py-6">
     <div class="container">
+      <div class="relative inset-y-10 items-center justify-between w-full flex lg:hidden ">
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            stroke="currentColor" class="h-4 w-4">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+          </svg>
+        </div>
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            stroke="currentColor" class="h-4 w-4">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+          </svg>
+        </div>
+      </div>
       <carousel ref="carouselRef" :wrap-around="true" :breakpoints="breakpoints">
         <slide :key="1">
           <div class="text-center">
-            <nuxt-img src="/icons/tag-sale.svg" class="mx-auto h-12 mb-2"></nuxt-img>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+              stroke="currentColor" class="mx-auto h-12 mb-2">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                d="m8.99 14.993 6-6m6 3.001c0 1.268-.63 2.39-1.593 3.069a3.746 3.746 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043 3.745 3.745 0 0 1-3.068 1.593c-1.268 0-2.39-.63-3.068-1.593a3.745 3.745 0 0 1-3.296-1.043 3.746 3.746 0 0 1-1.043-3.297 3.746 3.746 0 0 1-1.593-3.068c0-1.268.63-2.39 1.593-3.068a3.746 3.746 0 0 1 1.043-3.297 3.745 3.745 0 0 1 3.296-1.042 3.745 3.745 0 0 1 3.068-1.594c1.268 0 2.39.63 3.068 1.593a3.745 3.745 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.297 3.746 3.746 0 0 1 1.593 3.068ZM9.74 9.743h.008v.007H9.74v-.007Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm4.125 4.5h.008v.008h-.008v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+            </svg>
             <h3 class="font-semibold">Incomparable deals & rates</h3>
             <h4 class="text-sm">Extraordinary prices for your upcoming vacation experience.</h4>
           </div>
         </slide>
         <slide :key="2">
           <div class="text-center">
-            <nuxt-img src="/icons/call-people.svg" class="mx-auto h-12 mb-2"></nuxt-img>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+              stroke="currentColor" class="mx-auto h-12 mb-2">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z" />
+            </svg>
+
             <h3 class="font-semibold">24/7 Customer Service</h3>
             <h4 class="text-sm">Our travel specialists are just a call away, every day of the week.</h4>
           </div>
         </slide>
         <slide :key="3">
           <div class="text-center">
-            <nuxt-img src="/icons/cloud-check.svg" class="mx-auto h-12 mb-2"></nuxt-img>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+              stroke="currentColor" class="mx-auto h-12 mb-2">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
+            </svg>
+
             <h3 class="font-semibold">Thousands of testimonials</h3>
             <h4 class="text-sm">Our commitment to excellence is reflected in every positive review.</h4>
           </div>
         </slide>
         <slide :key="4">
           <div class="text-center">
-            <nuxt-img src="/icons/dollar-circle.svg" class="mx-auto h-12 mb-2"></nuxt-img>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+              stroke="currentColor" class="mx-auto h-12 mb-2">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+
             <h3 class="font-semibold">No Hidden fees</h3>
             <h4 class="text-sm">Extraordinary prices for your upcoming vacation experience.</h4>
           </div>
