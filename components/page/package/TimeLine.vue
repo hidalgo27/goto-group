a8ca85c7186779bbfa6a2af5521715e542a0f3ba
Julio <julio@goto.group>
2025-01-21 14:17:22 -0500

    fix: Resolve the order of the image with the activities and the font of the title

--- components/page/package/TimeLine.vue
+++ components/page/package/TimeLine.vue
@@ -27,7 +27,8 @@ const items = ref([
   <div class="container mx-auto px-4 space-y-12">
 
     <div class="text-center">
-      <h2 class="text-4xl font-bold text-gray-900">
+      <h2
+        class="text-gray-900 drop-shadow-[0_0_1px_rgba(255,255,255,0.5)] leading-tight text-5xl 2xl:text-7xl tracking-wide font-semibold font-playfair-display">
         Why you Should <span class="text-orange-500">choose Us</span>
       </h2>
       <p class="mt-4 text-gray-500 text-lg">
@@ -39,18 +40,23 @@ const items = ref([
 
     <div class="container mx-auto px-4">
       <div class="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
-        <div class="w-full">
+        <div class="w-full md:order-last">
           <img src="/images/hotel.jpg" alt="Image" class="w-full h-auto shadow-lg object-cover" />
         </div>
         <div class="space-y-6">
-          <div v-for="(item, index) in items" :key="index" class="flex items-start space-x-4">
-            <div class="relative w-16 text-center">
+          <div v-for="(item, index) in items" :key="index" class="flex items-start space-x-4 relative">
+            <!-- <div class="absolute left-[30px] transform -translate-x-1/2 
+                    top-8 sm:top-4 md:top-8
+                    h-28 md:h-32 lg:h-28
+                    border-l-2 border-dashed border-gray-300"></div> -->
+            <div class="relative w-16">
               <div class="py-2 font-bold text-xs text-gray-600">
                 <span class="rounded-full w-8 h-8 flex items-center justify-center text-white bg-gray-500">
                   {{ index + 1 }}
                 </span>
               </div>
             </div>
+
             <div class="flex-1">
               <h3 class="text-lg font-semibold text-gray-800">
                 {{ item.title }}
@@ -63,6 +69,7 @@ const items = ref([
         </div>
       </div>
     </div>
+
   </div>
 </template>
 
